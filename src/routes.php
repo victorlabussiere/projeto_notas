fe6ca6547db6a8d87e8ece2ce1baf7e52c5f6c93
Victor Labussiere <victor.labussiere95@gmail.com>
2023-08-14 23:09:23 -0300

    router updated

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,30 @@
 <?php
 
 $path = [
-    'home' => ['path' => './src/controllers/home.php', 'heading' => 'Home'],
-    'about' => ['path' => './src/controllers/about.php', 'heading' => 'Sobre nós'],
-    'contact' => ['path' => './src/controllers/contact.php', 'heading' => 'Contato'],
-    'notes' => ['path' => './src/controllers/notes/index.php', 'heading' => 'Notas'],
-    'note' => ['path' => './src/controllers/notes/show.php', 'heading' => 'Nota'],
-    'note_create' => ['path' => './src/controllers/notes/create.php', 'heading' => 'Nota']
+    'home' => [
+        'path' => './src/controllers/home.php',
+        'heading' => 'Home'
+    ],
+    'about' => [
+        'path' => './src/controllers/about.php',
+        'heading' => 'Sobre nós'
+    ],
+    'contact' => [
+        'path' => './src/controllers/contact.php',
+        'heading' => 'Contato'
+    ],
+    'notes' => [
+        'path' => './src/controllers/notes/index.php',
+        'heading' => 'Notas'
+    ],
+    'show' => [
+        'path' => './src/controllers/notes/show.php',
+        'heading' => 'Nota'
+    ],
+    'create' => [
+        'path' => './src/controllers/notes/create.php',
+        'heading' => 'Nova Nota'
+    ]
 ];
 
 return $routes = [
@@ -14,6 +32,6 @@ return $routes = [
     '/about' => $path['about'],
     '/contact' => $path['contact'],
     '/notes' => $path['notes'],
-    '/note' => $path['note'],
-    '/create' => $path['note_create']
+    '/note' => $path['show'],
+    '/create' => $path['create']
 ];
