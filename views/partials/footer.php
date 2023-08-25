439dc0991311ef9075d0e3208d532a727b7b77d0
Victor Labussiere <victor.labussiere95@gmail.com>
2023-08-25 11:42:28 -0300

    partials structure changes

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,7 @@
 <footer class='flex items-center justify-center bg-lime-300 text-lime-600 w-screen py-6 self-end bottom-0 left-0'>
     <p>Desenvolvido por <a href="https://github.com/victorlabussiere" target='_blank' class='hover:underline hover:text-lime-800 font-semibold'>Victor Labussiere</a></p>
 </footer>
+
+</body>
+
+</html>
--- views/partials/head.php
+++ views/partials/head.php
@@ -1,7 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./output.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link rel="stylesheet" href="/output.css">
+
     <title>Notas | <?= $heading ?></title>
 </head>
+
+<body class='flex flex-col w-screen h-screen justify-between p-0 relative'>
