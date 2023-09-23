0263f8d12958533005f4c36ccaee7f798aa4bddc
Victor Labussiere <victor.labussiere95@gmail.com>
2023-09-23 19:18:41 -0300

    unauthorized and not found pages names fixed

---
+++ views/pages/404.view.php
@@ -0,0 +1,13 @@
+<?php
+require base_path('views/partials/head.php');
+require base_path('views/partials/topbar.php');
+?>
+<main class="h-full w-full flex flex-col items-center pt-20 py-10">
+    <section>
+        <h2 class="font-bold text-3xl text-lime-600"> Not Found </h2>
+        <a href="/" class='text-lime-700 font-bold text-base hover:text-lime-800 hover:underline'>
+            Voltar a página inicial
+        </a>
+    </section>
+</main>
+<?php require base_path('views/partials/footer.php') ?>
