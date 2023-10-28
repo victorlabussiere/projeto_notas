f738cecf3ff2f934085817dff25b7f966beb8ca0
Victor Labussiere <victor.labussiere95@gmail.com>
2023-10-28 16:35:26 -0300

    login route

--- routes.php
+++ routes.php
@@ -7,22 +7,27 @@ App::bind('Core\Router', function () {
     $router = new Router();
 
     // OTHER PAGES ROUTER
-    $router->get('/', 'controllers/index.php');
-    $router->get('/about', 'controllers/about.php');
-    $router->get('/contact', 'controllers/contact.php');
+    $router->get('/', 'index.php');
+    $router->get('/about', 'about.php');
+    $router->get('/contact', 'contact.php');
 
     // NOTES 
-    $router->get('/notes', 'controllers/notes/index.php')->only('auth');
-    $router->get('/note', 'controllers/notes/show.php');
-    $router->delete('/note', 'controllers/notes/destroy.php');
-    $router->get('/notes/create', 'controllers/notes/create.php');
-    $router->post('/notes/create', 'controllers/notes/store.php');
-    $router->get('/note/edit', 'controllers/notes/edit.php');
-    $router->patch('/note', 'controllers/notes/update.php');
+    $router->get('/notes', 'notes/index.php')->only('auth');
+    $router->get('/note', 'notes/show.php');
+    $router->delete('/note', 'notes/destroy.php');
+    $router->get('/notes/create', 'notes/create.php');
+    $router->post('/notes/create', 'notes/store.php');
+    $router->get('/note/edit', 'notes/edit.php');
+    $router->patch('/note', 'notes/update.php');
 
     // REGISTER ROUTER
-    $router->get('/register', 'controllers/register/create.php')->only('guest');
-    $router->post('/register', 'controllers/register/store.php');
+    $router->get('/register', 'register/create.php')->only('guest');
+    $router->post('/register', 'register/store.php');
+
+    // LOGIN SESSION
+    $router->get('/session', 'session/create.php')->only('guest');
+    $router->post('/session', 'session/store.php')->only('guest');
+    $router->delete('/session', 'session/destroy.php')->only('auth');
 
     return $router;
 });
